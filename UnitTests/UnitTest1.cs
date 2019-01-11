c2398b80fd964e8f7369136238ff38bc6f7c6e75
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-01-10 20:03:28 -0800

    set up project and test suite with proper external refs

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using WordGuessGame;
+
+namespace UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ WordGuessGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+
+namespace WordGuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
